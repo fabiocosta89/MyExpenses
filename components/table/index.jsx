088c167b142f69eb9a17bb08a0cc57954a686565
Fábio Costa <fabio.costa-prestataire@docaposte.fr>
2021-11-25 14:17:18 +0100

    Add React Table; Use react table to generate tables

--- components/table/index.jsx
+++ components/table/index.jsx
@@ -1,22 +1,61 @@
 import React from 'react';
+import { useTable } from 'react-table'
 
-const ResponsiveHoverTable = ({cardTitle, head, body}) => {
+const ResponsiveHoverTable = ({cardTitle, columns, data}) => {
+    const tableInstance = useTable({ columns, data });
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance;
+
     return (
         <div className="card">
             <div className="card-header">
                 <h3 className="card-title">{cardTitle}</h3>
             </div>
             <div className="card-body table-responsive p-0">
-                <table className="table table-hover text-nowrap">
+                <table className="table table-hover text-nowrap" {...getTableProps()}>
                     <thead>
-                        <tr>
-                            {head.map((value, index) => {
-                                return <th key={index}>{value}</th>
-                            })}
-                        </tr>
+                        {
+                            headerGroups.map((headerGroup, index) => (
+                                <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+                                    {
+                                        headerGroup.headers.map((column, index) => (
+                                            <th {...column.getHeaderProps()} key={index}>
+                                            {
+                                                column.render('Header')
+                                            }
+                                            </th>
+                                        ))
+                                    }
+                                </tr>
+                            ))
+                        }
                     </thead>
-                    <tbody>
-                        
+                    <tbody {...getTableBodyProps()}>
+                        {
+                            rows.map((row, index) => {
+                                prepareRow(row)
+                                return (
+                                <tr {...row.getRowProps()} key={index}>
+                                    {
+                                    row.cells.map((cell, index) => {
+                                    
+                                    return (
+                                        <td {...cell.getCellProps()} key={index}>
+                                        {
+                                            cell.render('Cell')
+                                        }
+                                        </td>
+                                    )
+                                    })}
+                                </tr>
+                                )
+                            })}
                     </tbody>
                 </table>
             </div>
--- pages/expenses/categories/index.jsx
+++ pages/expenses/categories/index.jsx
@@ -3,9 +3,59 @@ import Head from 'next/head';
 
 import PageHeader from '../../../components/page-header';
 import BreadcrumbItem from '../../../components/breadcrumb/breadcrumb-item';
+import ResponsiveHoverTable from '../../../components/table';
 
 const Categories = () => {
     const title = "Expenses Categories";
+    const data = React.useMemo(
+        () => [
+            {
+                id: '1',
+                name: 'Test 1',
+                action: <>
+                    <a className="btn btn-info btn-sm" href="#">
+                        <i className="fas fa-pencil-alt">
+                        </i>
+                        Edit
+                    </a>
+                    <a className="btn btn-danger btn-sm" href="#">
+                        <i className="fas fa-trash">
+                        </i>
+                        Delete
+                    </a>
+                </>,
+            },
+            {
+                id: '2',
+                name: 'Test 2',
+                action: <a />,
+            },
+            {
+                id: '3',
+                name: 'Test 3',
+                action: <a />,
+            },
+        ],
+        []
+    );
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'ID',
+                accessor: 'id',
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+            },
+            {
+                Header: '',
+                accessor: 'action',
+            },
+        ],
+        []
+    );
 
     return (
         <>
@@ -20,7 +70,11 @@ const Categories = () => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-12">
-                            
+                            <ResponsiveHoverTable 
+                                cardTitle="Categories"
+                                columns={columns}
+                                data={data}
+                            />
                         </div>
                     </div>
                 </div>
